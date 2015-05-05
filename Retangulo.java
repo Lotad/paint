b25607e8bd8dd0f19febaa6a69371431d80493ff
Lotad <inutuni1@hotmail.com>
2015-05-05 10:19:55 -0300

    Update Retangulo.java

--- Retangulo.java
+++ Retangulo.java
@@ -4,21 +4,18 @@ import java.util.*;
 public class Retangulo extends Figura {
     protected Ponto p1, p2;
 	
-    public Retangulo (int x1, int y1, int x2, int y2)
-    {
+    public Retangulo (int x1, int y1, int x2, int y2){
         this (x1, y1, x2, y2, Color.BLACK);
     }
 	
-    public Retangulo (int x1, int y1, int x2, int y2, Color cor)
-    {
+    public Retangulo (int x1, int y1, int x2, int y2, Color cor){
         super(cor);
 
         this.p1 = new Ponto (x1,y1,cor);
         this.p2 = new Ponto (x2,y2,cor);
     }
 
-    public Retangulo (String s)
-    {
+    public Retangulo (String s){
         StringTokenizer quebrador = new StringTokenizer(s,":");
 
         quebrador.nextToken();
@@ -38,28 +35,23 @@ public class Retangulo extends Figura {
         this.cor = cor;
     }
 
-    public void setP1 (int x, int y)
-    {
+    public void setP1 (int x, int y){
         this.p1 = new Ponto (x,y,this.getCor());
     }
 
-    public void setP2 (int x, int y)
-    {
+    public void setP2 (int x, int y){
         this.p2 = new Ponto (x,y,this.getCor());
     }
 
-    public Ponto getP1 ()
-    {
+    public Ponto getP1 (){
         return this.p1;
     }
 
-    public Ponto getP2 ()
-    {
+    public Ponto getP2 (){
         return this.p2;
     }
 
-    public void torneSeVisivel (Graphics g)
-    {
+    public void torneSeVisivel (Graphics g){
         g.setColor(this.cor);
         g.drawLine(this.p1.getX(), this.p1.getY(),   // ponto inicial
                    this.p1.getX(), this.p2.getY());
@@ -71,8 +63,7 @@ public class Retangulo extends Figura {
                    this.p2.getX(), this.p2.getY());// ponto final
     }
 
-    public String toString()
-    {
+    public String toString(){
         return "r:" +
                this.p1.getX() +
                ":" +
