b746e5b54e99e6a8df9cd65e062388038e22da61
Lotad <inutuni1@hotmail.com>
2015-05-04 21:47:24 -0300

    figura

---
+++ Figura.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+
+public abstract class Figura // implements Cloneable
+{
+    protected Color cor;
+    protected Color preen;
+
+    protected Figura () {
+        this (Color.BLACK);
+    }
+
+    protected Figura (Color cor) {
+        this (cor, Color.WHITE);
+    }
+
+    protected Figura (Color cor, Color preen) {
+        this.setCor (cor, preen);
+    }
+
+    public void setCor (Color cor, Color preen) {
+        this.cor = cor;
+        this.preen = preen;
+    }
+
+    public Color getCor() {
+        return this.cor;
+    }
+
+    public Color getPreen() {
+        return this.preen;
+    }
+
+  //public abstract Object  clone          ();
+  //public abstract boolean equals         (Object obj);
+  //public abstract int     hashCode       ();
+    public abstract String  toString       ();
+    public abstract void    torneSeVisivel (Graphics g);
+}
